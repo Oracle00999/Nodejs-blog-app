ea335593eb8cca543da89b662d29d4d838332187
Oracle00999 <nwazotachibuike@gmail.com>
2025-10-08 19:06:13 +0100

    Fix Post model redefinition issue

--- models/post.js
+++ models/post.js
@@ -19,6 +19,6 @@ const postSchema = new mongoose.Schema({
   },
 });
 
-const Post = mongoose.model("Post", postSchema);
+const Post = mongoose.models.Post || mongoose.model("Post", postSchema);
 
 module.exports = Post;
